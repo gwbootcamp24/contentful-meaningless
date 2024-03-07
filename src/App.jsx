667f70a7e8bdb8d21bf21afaf3f64aecf5d85e1c
Georg <gwbootcamp24.google.com>
2024-03-08 00:28:34 +0100

    igdb: Read games api and change genre from genres endpoint

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,14 @@
 import { useState } from 'react'
 import Collection from './components/Collection'
+import Twitchtest from './components/Twitchtest'
 import './App.css'
 
 function App() {
   
   return (
     <>
-       <Collection />
+       {/* <Collection /> */}
+       <Twitchtest />
      </>
   )
 }
